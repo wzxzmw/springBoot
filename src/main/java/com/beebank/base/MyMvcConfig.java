922711d41add2c69b9c390c612ede21e6d60b0d6
wangzx <wangzx51@126.com>
2020-11-18 18:11:24 +0800

    提交测试

---
+++ src/main/java/com/beebank/base/MyMvcConfig.java
@@ -0,0 +1,18 @@
+package com.beebank.base;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+public class MyMvcConfig {
+	public InternalResourceViewResolver viewResovler(){
+		InternalResourceViewResolver vierResovler = new InternalResourceViewResolver();
+		vierResovler.setPrefix("/WEB-INF/classes/views");
+		vierResovler.setSuffix(".jsp");
+		vierResovler.setViewClass(JstlView.class);
+		return vierResovler;
+	}
+}
