a76e020e8b18f96f4662c721d939146c831f8076
wangzx51 <wangzx51@126.com>
2020-11-27 17:53:19 +0800

    提交修改

--- src/main/java/com/beebank/base/WebMvcAdapter.java
+++ src/main/java/com/beebank/base/WebMvcAdapter.java
@@ -3,7 +3,7 @@ package com.beebank.base;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
+@SuppressWarnings("all")
 @EnableWebMvc
 @ControllerAdvice
 public class WebMvcAdapter extends WebMvcConfigurerAdapter{
